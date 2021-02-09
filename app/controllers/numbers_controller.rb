c1cbcfd7a83068c63e36dc042ca0be24e83b17b3
mgrayhack <mgrayhack@chicagobooth.edu>
2021-02-09 17:23:56 +0000

    Fortune Teller aries

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,15 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+  def winners_losers
+    @list_unlucky = Array.new
+
+    5.times do
+      fil_with_num = rand(1...100)
+      
+      @list_unlucky.push(fil_with_num)
+    end
+
+    render({ :template => "lottery_stuff/shoot.html.erb"})
+  end
 end
